dcc7f578c943f19d8aadc3cac4abde1c6a10f57f
bhavyabatra <bhavyabatra88@gmail.com>
2016-10-12 15:08:43 -0500

    Resolving Key Duplication Issue

--- SWEN_Dynamo/Controllers/UsersController.cs
+++ SWEN_Dynamo/Controllers/UsersController.cs
@@ -10,25 +10,46 @@ using Amazon.MachineLearning.Model;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Runtime;
 using System.Collections.Concurrent;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.SecurityToken;
 
 namespace SWEN_Dynamo.Controllers
 {
     public class UsersController : Controller
     {
-   
-
-
         public ActionResult Index(UserModel model)
         {
-           
+
             AmazonDynamoDBClient client = new AmazonDynamoDBClient();
             string tablename = "User";
-        var request = new PutItemRequest
+            var verify = new GetItemRequest
             {
                 TableName = tablename,
-                Item = new Dictionary<string, AttributeValue>()
+                Key = new Dictionary<string, AttributeValue>()
+        {
+          { "USID", new AttributeValue { N = Convert.ToString(model.USID) } }
+        },
+
+                ProjectionExpression = "Email, Datecreated, Datecreated, Datemodified,FirstName,LastName,Password,Vcode,RID,RA,FA,SA,#R ,Phone",
+                ConsistentRead = true
+            } ;
+            var check = client.GetItem(verify);
+            if (check != null)
+            {
+                return View();
+            }
+
+            else
+            {
+                var request = new PutItemRequest
+                {
+                    TableName = tablename,
+                    //    Key = new Dictionary<string, AttributeValue>()
+                    //{ { "USID", new AttributeValue { N = Convert.ToString(model.USID)  } } },
+
+                    Item = new Dictionary<string, AttributeValue>()
       {
-          { "USID", new AttributeValue { N = Convert.ToString(model.USID)  }},
+          { "USID", new AttributeValue { N = Convert.ToString(model.USID) } },
           { "Email", new AttributeValue { S = model.Email }},
           { "Datecreated", new AttributeValue { S = Convert.ToString(model.Datecreated) }},
           { "Datemodified", new AttributeValue { S = Convert.ToString(model.Datemodified) }},
@@ -42,10 +63,45 @@ namespace SWEN_Dynamo.Controllers
           { "SA", new AttributeValue { N = Convert.ToString(model.SA) }},
           { "Region", new AttributeValue { S = model.Region }},
           { "Phone", new AttributeValue { N = Convert.ToString(model.Phone) }},
-      }
-            };
-            client.PutItem(request);
-            return View();
-        }
-    }
-}
+                    }
+                };
+                client.PutItem(request);
+                return View();
+
+            }
+        } } }
+        
+    
+
+
+
+
+//        var request = new PutItemRequest
+//            {
+//                TableName = tablename,
+//                 Key = new Dictionary<string, AttributeValue>()
+//                { { "USID", new AttributeValue { N = Convert.ToString(model.USID)  } } },
+
+//            Item = new Dictionary<string, AttributeValue>()
+//      {
+         
+//          { "Email", new AttributeValue { S = model.Email }},
+//          { "Datecreated", new AttributeValue { S = Convert.ToString(model.Datecreated) }},
+//          { "Datemodified", new AttributeValue { S = Convert.ToString(model.Datemodified) }},
+//          { "FirstName", new AttributeValue { S = model.Firstname }},
+//          { "LastName", new AttributeValue { S = model.Lastname }},
+//          { "Password", new AttributeValue { S = model.Password }},
+//          { "Vcode", new AttributeValue { S = model.Vcode }},
+//          { "RID", new AttributeValue { N = Convert.ToString(model.RID) }},
+//          { "RA", new AttributeValue { N = Convert.ToString(model.RA) }},
+//          { "FA", new AttributeValue { N = Convert.ToString(model.FA) }},
+//          { "SA", new AttributeValue { N = Convert.ToString(model.SA) }},
+//          { "Region", new AttributeValue { S = model.Region }},
+//          { "Phone", new AttributeValue { N = Convert.ToString(model.Phone) }},
+//      }
+//            };
+//            client.PutItem(request);
+//            return View();
+//        }
+//    }
+//}
