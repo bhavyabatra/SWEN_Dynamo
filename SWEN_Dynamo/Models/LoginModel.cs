094028e042a4e5e76df39d380208a9dd85cd5b38
bhavyabatra <bhavyabatra88@gmail.com>
2016-10-18 17:20:00 -0500

    Logins and Roles Indexes.

--- SWEN_Dynamo/Models/LoginModel.cs
+++ SWEN_Dynamo/Models/LoginModel.cs
@@ -7,5 +7,15 @@ namespace SWEN_Dynamo.Models
 {
     public class LoginModel
     {
+
+        public int USID { get; set; }
+
+        public string Password { get; set; } = "P@33w0rd";
+
+        public string Vcode { get; set; } = "01001010";
+
+        public int RID { get; set; }
+
+        public string Email { get; set; } = "Default@s.com";
     }
 }
