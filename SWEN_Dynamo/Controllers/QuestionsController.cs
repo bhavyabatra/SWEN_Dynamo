556549157659d46ee497d39fa69a8a53a00246aa
bhavyabatra <bhavyabatra88@gmail.com>
2016-10-25 16:35:06 -0500

    Now Managing Questions DB. Yet to add options.

---
+++ SWEN_Dynamo/Controllers/QuestionsController.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using SWEN_Dynamo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SWEN_Dynamo.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        // GET: Questions
+        public ActionResult ManageObjectivesAndQuestions(QuestionsModel mod)
+        {
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+            string tablename = "QuestionsDB";
+            var request = new PutItemRequest
+            {
+                TableName = tablename,
+                Item = new Dictionary<string, AttributeValue>()
+      {
+          { "OID", new AttributeValue { N = Convert.ToString(mod.OID) } },
+          { "Objective", new AttributeValue { S = mod.Objective }},
+          { "QID", new AttributeValue { N = Convert.ToString(mod.QID) }},
+          { "Question", new AttributeValue { S = mod.Question }},
+          { "SurveyType", new AttributeValue { S = mod.SurveyType }},
+     }
+            };
+            client.PutItem(request);
+            return View();
+        }
+    }
+}
---
+++ SWEN_Dynamo/Models/QuestionsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SWEN_Dynamo.Models
+{
+    public class QuestionsModel
+    {
+        // Manage Questions Database 
+        // Include Options 
+    // Option 1.2.3.4/5
+    // Add in View and then bind to model. 
+        [Required(ErrorMessage = "SurveyType is required")]
+        //  [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string SurveyType { get; set; } = "Default";
+
+        public int OID { get; set; }
+
+        [Required(ErrorMessage = "Obective is required")]
+     //  [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string Objective { get; set; } = "Default";
+
+        public int QID { get; set; }
+
+       [Required(ErrorMessage = "Question is required")]
+      //  [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string Question { get; set; } = "Default";
+
+
+    }
+}
