01d6ead0cd955af9bff73f5d6caa558ecd2a7a98
bhavyabatra <bhavyabatra88@gmail.com>
2016-11-15 20:45:05 -0600

    User Lists

--- SWEN_Dynamo/Controllers/UsersController.cs
+++ SWEN_Dynamo/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ namespace SWEN_Dynamo.Controllers
     {
             
 
-        public ActionResult Index(UserModel model)
+        public ActionResult Create(UserModel model)
         {
             // string keyId = "arn:aws:dynamodb:us-west-2:964019329379:table/User";
             // String pass = model.Password;
@@ -32,8 +32,8 @@ namespace SWEN_Dynamo.Controllers
 
             var keyNew = Helper.GeneratePassword(10);
             var pass = Helper.EncodePassword(model.Password, keyNew);
-            //model.Password = pass;
-            //model.Vcode = keyNew;
+            model.Password = pass;
+            model.Vcode = keyNew;
             
             AmazonDynamoDBClient client = new AmazonDynamoDBClient();
           
@@ -63,39 +63,36 @@ namespace SWEN_Dynamo.Controllers
                 return View();
 
             }
-        } } 
+
+        // GET: Users
+        static List<UserModel> people = new List<UserModel>();
+        public ActionResult UsersList()
+        {
+            UserModel xx = new UserModel();
+            xx.USID = 155;
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            // Table table = new Table("Logins");
+            Table table = Table.LoadTable(client, "User");
+            GetItemOperationConfig config = new GetItemOperationConfig()
+            {
+                AttributesToGet = new List<string>() { "RID" },
+            };
+            Document document = table.GetItem(xx.USID, config);
+            if (document != null)
+            {
+                Document docs = table.GetItem(xx.USID, config);
+                {
+                    people.Add(new UserModel() { USID = xx.USID, RID = Convert.ToInt32(docs["RID"]) });
+                }
+            }
+            return View(people);
+        }
+    }
+}
+        
         
     
 
 
 
 
-//        var request = new PutItemRequest
-//            {
-//                TableName = tablename,
-//                 Key = new Dictionary<string, AttributeValue>()
-//                { { "USID", new AttributeValue { N = Convert.ToString(model.USID)  } } },
-
-//            Item = new Dictionary<string, AttributeValue>()
-//      {
-         
-//          { "Email", new AttributeValue { S = model.Email }},
-//          { "Datecreated", new AttributeValue { S = Convert.ToString(model.Datecreated) }},
-//          { "Datemodified", new AttributeValue { S = Convert.ToString(model.Datemodified) }},
-//          { "FirstName", new AttributeValue { S = model.Firstname }},
-//          { "LastName", new AttributeValue { S = model.Lastname }},
-//          { "Password", new AttributeValue { S = model.Password }},
-//          { "Vcode", new AttributeValue { S = model.Vcode }},
-//          { "RID", new AttributeValue { N = Convert.ToString(model.RID) }},
-//          { "RA", new AttributeValue { N = Convert.ToString(model.RA) }},
-//          { "FA", new AttributeValue { N = Convert.ToString(model.FA) }},
-//          { "SA", new AttributeValue { N = Convert.ToString(model.SA) }},
-//          { "Region", new AttributeValue { S = model.Region }},
-//          { "Phone", new AttributeValue { N = Convert.ToString(model.Phone) }},
-//      }
-//            };
-//            client.PutItem(request);
-//            return View();
-//        }
-//    }
-//}
