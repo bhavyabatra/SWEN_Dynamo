647d79c8969ff43064a06b8e7daccda1ec989275
bhavyabatra <bhavyabatra88@gmail.com>
2017-03-15 17:07:48 -0500

    Take Survey Stage 4

--- SWEN_Dynamo/Controllers/TestController.cs
+++ SWEN_Dynamo/Controllers/TestController.cs
@@ -13,26 +13,90 @@ namespace SWEN_Dynamo.Controllers
     public class TestController : Controller
     {
         // GET: Test
-        public ActionResult TestView()
+        public ActionResult TestView(TestModel mod, string SaveSurvey)
         {
-            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-            string tablename = "Respondent";
-            Table table1 = Table.LoadTable(client, "Respondent");
-            TestModel mod = new TestModel();
-            var item = table1.GetItem("Test Four", "444@4.com");
-            //  var result = client.GetItem(request);
-            
+            List<SelectListItem> listSelectListItems = new List<SelectListItem>() {
+
+                new SelectListItem() {Value="1", Text="Strong Agree" },
+                new SelectListItem() {Value="2", Text="Agree" }
+            };
             List<TestModel> tm = new List<TestModel>();
-              // Issue request
-             // foreach (var kvp in itme.)
+            tm.Add(new TestModel() { AnswerOptions = listSelectListItems });
+
+            //.......................
+            //AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            //string tablename = "Respondent";
+            //Table table1 = Table.LoadTable(client, "Respondent");
+
+
+            //var item = table1.GetItem("Test Four", "444@4.com");
+            //  var result = client.GetItem(request);
+
+            //List<TestModel> tm = new List<TestModel>();
+            //List<TestModel> am = new List<TestModel>();
+            // Issue request
+            // foreach (var kvp in itme.)
+            //     {//if (item["CQ1"] != null)
+            //      {
+            //    List<SelectListItem> list = new List<SelectListItem>() {
+            //new SelectListItem(){ Value="1", Text="ActionScript"}, };
+            //    mod = new TestModel();
+            //    mod.Answer = new SelectList(list, "Value", "Text");
+            //mod.tm.Add(new TestModel() { test = "Hello" });
+            //  return View(mod.tm);
+
+            //    }
+            //am.Add(new TestModel { Answer = "Strongly Agree" });
+            //am.Add(new TestModel { Answer = "Great" });
+            //am.Add(new TestModel { Answer = "Well" });
+            //  }
+            // ViewData["DropDownSource"] = am;
+            // return View(tm);
+            //TestModel citiesViewModel = new TestModel()
+            //{
+            //if (!string.IsNullOrWhiteSpace(SaveSurvey))
+            //{
+            //    //};
+            //    Console.WriteLine("Surcess");
+            //}
+            return View(tm);
+        }
+
+        [HttpPost, ActionName("TestView")]
+        public ActionResult TestViewConfirm(TestModel mod, string SaveSurvey, string Answer)
+        {
+
+            List<TestModel> tm = new List<TestModel>();
+            if (!string.IsNullOrWhiteSpace(SaveSurvey))
             {
-               
-                tm.Add(new TestModel { test = "Hello" });
-                tm.Add(new TestModel { test = "Hi" });
-                tm.Add(new TestModel { test = "Great" });
-                tm.Add(new TestModel { test = "Well" });
+                {
+                List<SelectListItem> listSelectListItems = new List<SelectListItem>() {
+
+                new SelectListItem() {Value="1", Text="Strong Agree" },
+                new SelectListItem() {Value="2", Text="Agree" }
+            };
+
+                tm.Add(new TestModel() { ans = Convert.ToString(Answer) });
+
+              
+                    if (Answer == "Strong Agree")
+                    {
+                        return View("TestCheck");
+                    }
+                }
+                //  Console.WriteLine(mod.Answer);
             }
             return View(tm);
         }
+
+        public ActionResult TestCheck()
+        {
+            return View();
+        }
+
     }
 }
+
+       
+ 
+    
--- SWEN_Dynamo/Models/TestModel.cs
+++ SWEN_Dynamo/Models/TestModel.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SWEN_Dynamo.Models
 {
     public class TestModel
     {
-        public string test { get; set; }
+        public string test { get; set; } = "Strongly Agree";
+
+        public string Answer { get; set; }
+        public IEnumerable<SelectListItem> AnswerOptions { get; set; }
+        public string ans { get; set; } = "Agree";
     }
+
+    // public List<TestModel> tm = new List<TestModel>();
+
+    // public IEnumerable<SelectListItem> Answers { get; set; }
+
 }
+    
